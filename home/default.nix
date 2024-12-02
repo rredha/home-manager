aeaf499e706a5a7e0096b36ab714129e27cfd0a5
redha <ahred.mbrk@gmail.com>
2024-12-02 12:20:22 +0100

    Adding Openssl before installing Unityhub.

--- home/default.nix
+++ home/default.nix
@@ -91,6 +91,7 @@
   # The home.packages option allows you to install Nix packages into your
   # environment.
   home.packages = with pkgs; [
+      openssl
       orca-slicer
       unzip
       zotero
