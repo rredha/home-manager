ea733d6fce1518b8ab0dfe071b894dde183853d5
redha <ahred.mbrk@gmail.com>
2024-12-02 12:22:41 +0100

    Adding unityhub

--- home/default.nix
+++ home/default.nix
@@ -91,6 +91,7 @@
   # The home.packages option allows you to install Nix packages into your
   # environment.
   home.packages = with pkgs; [
+      unityhub
       openssl
       orca-slicer
       unzip
